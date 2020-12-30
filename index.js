36c0caf0046ef81216c571cb5840c42b065ebaca
Derek Yen <cyen@Derekde-MacBook-Pro.local>
2020-12-31 01:25:45 +0800

    feat: 新增 teamPitcherStatsAPI

--- index.js
+++ index.js
@@ -16,5 +16,6 @@ app.get('/standingsApi/:year/:season', apisController.getStandings)
 app.get('/batterStatsApi/:year/:statsType/:sort', apisController.getBatterStats)
 app.get('/pitcherStatsApi/:year/:statsType/:sort', apisController.getPitcherStats)
 app.get('/teamBatterStatsApi/:year/:statsType/:sort', apisController.getTeamBatterStats)
+app.get('/teamPitcherStatsApi/:year/:statsType/:sort', apisController.getTeamPitcherStats)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
